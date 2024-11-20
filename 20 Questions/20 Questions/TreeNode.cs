e8f0d3000e1ecf9c8e4bc683b47a5a50051bb60f
Olivia Gauthier <org1181@rit.edu>
2024-11-20 12:24:30 -0500

    Created the .gitignore

---
+++ 20 Questions/20 Questions/TreeNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class TreeNode
+{
+	public string data;
+	public Boolean yes;
+	public Boolean no;
+
+	//represents each node in the question tree
+	public TreeNode(string newData, Boolean newYes, Boolean newNo)
+	{
+		data = newData;
+		yes = newYes;
+		no = newNo;
+	}
+}
