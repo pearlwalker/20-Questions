7ab43c7f8d3dcd3a8405e460a77d59578ae7ddd4
Olivia Gauthier <org1181@rit.edu>
2024-11-20 13:03:23 -0500

    added more methods
    added query(), onQueryObject(), updateTree() but getYesOrNo() is not declared

--- 20 Questions/20 Questions/TreeNode.cs
+++ 20 Questions/20 Questions/TreeNode.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 
 public class TreeNode
 {
@@ -46,10 +50,9 @@ public class TreeNode
 	}
 
     /*
- * Notes:
- *	When a node has two null references the computer has no more questions and the computer is ready to make a guess.
- *	The following method will check to see if a node is a leaf node and it will be used later when progressing through the binary trees. - https://www.c-sharpcorner.com/UploadFile/4a950c/20-questions-guessing-game-using-binary-trees/
- */
+     * When a node has two null references the computer has no more questions and the computer is ready to make a guess.
+     * The following method will check to see if a node is a leaf node and it will be used later when progressing through the binary trees. - https://www.c-sharpcorner.com/UploadFile/4a950c/20-questions-guessing-game-using-binary-trees/
+	*/
 	public bool isQuestion()
 	{
 		if (noNode == null && yesNode == null)
@@ -58,6 +61,62 @@ public class TreeNode
 		} else
 		{
 			return true;
+		}
+	}
+
+	/*
+	 * This game requires a user to answer a question. If the user answers yes, it asks the next question contained in the yesNode. To access the next node we are going to use a method called query(). To access the yes node use yesNode.query() and noNode for the no node. This method will also have to reconstruct the tree when the computer does not know the answer. This is done in the updateTree() method. 
+	 */
+	public void query()
+	{
+		if (this.isQuestion())
+		{
+			Console.WriteLine(this.message);
+			Console.Write("enter 'y' for yes and 'n' for no: ");
+			char input = getYesOrNo();
+			if (input == 'y')
+			{
+				yesNode.query();
+			} else
+			{
+				noNode.query();
+			}
+		} else
+		{
+			this.onQueryObject();
+		}
+	}
+
+	public void onQueryObject()
+	{
+		Console.WriteLine("Are you thinking of a(n) " + this.message + "?");
+		Console.Write("Enter 'y' for yes and 'n' for no: ");
+		char input = getYesOrNo();
+		if (input == 'y')
+		{
+			Console.WriteLine("The Computer Wins\n");
+		} else
+		{
+			updateTree();
+		}
+	}
+
+	private void updateTree()
+	{
+		Console.Write("You win! What were you thinking of?");
+		string userObject = Console.ReadLine();
+		Console.Write("Please enter a question to distinguish a(n) " + this.message + " from " + userObject + ": ");
+		string userQuestion = Console.ReadLine();
+		Console.Write("If you were thinking of a(n) " + userObject + ", what would the answer to that question be?");
+		char input = getYesOrNo();
+		if (input == 'y')
+		{
+			this.noNode = new TreeNode(this.message);
+			this.yesNode = new TreeNode(userObject);
+		} else
+		{
+			this.yesNode = new TreeNode(this.message);
+			this.noNode = new TreeNode(userObject);
 		}
 	}
 
