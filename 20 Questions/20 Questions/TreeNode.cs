a6167621ec62ac788b756b747138027f31e63b64
Olivia Gauthier <org1181@rit.edu>
2024-11-20 12:48:12 -0500

    Added mutator methods to TreeNode.cs

--- 20 Questions/20 Questions/TreeNode.cs
+++ 20 Questions/20 Questions/TreeNode.cs
@@ -2,15 +2,45 @@
 
 public class TreeNode
 {
-	public string data;
-	public Boolean yes;
-	public Boolean no;
+	string data; //holds the question or answer
+	TreeNode yesNode; //reference to node if user enters no
+	TreeNode noNode; //reference to node user enters yes
 
 	//represents each node in the question tree
-	public TreeNode(string newData, Boolean newYes, Boolean newNo)
+	public TreeNode(string newData, TreeNode newYes, TreeNode newNo)
 	{
 		data = newData;
-		yes = newYes;
-		no = newNo;
+		yesNode = newYes;
+		noNode = newNo;
+	}
+
+	public void setData(string nodeData)
+	{
+		data = nodeData;
+	}
+
+	public string getData()
+	{
+		return data;
+	}
+
+	public void setNoNode(TreeNode node)
+	{
+		noNode = node.noNode;
+	}
+
+	public TreeNode getNoNode()
+	{
+		return noNode;
+	}
+
+	public void setYesNode(TreeNode node)
+	{
+		yesNode = node;
+	}
+
+	public TreeNode getYesNode()
+	{
+		return yesNode;
 	}
 }
