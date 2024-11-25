91091200a8edf073de26c8a3c83a3c6775737b67
EcliS-e <marianita345@icloud.com>
2024-11-24 19:48:13 -0500

    Commented out the play again function
    It doesn't work, and I don't think it's essential for the project.

--- 20 Questions/20 Questions/obj/Debug/net8.0/20 Questions.AssemblyInfo.cs
+++ 20 Questions/20 Questions/obj/Debug/net8.0/20 Questions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("20 Questions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b4eddd1065bdf4e0bdafb095edf335eb27847c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10a7be582901934a0afefbe56454edacb5e0eda8")]
 [assembly: System.Reflection.AssemblyProductAttribute("20 Questions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("20 Questions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
